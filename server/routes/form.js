1e0f1f709db733a88b9612e224c13dc6f14325a2
mattwhitfield <whitfield.mj@gmail.com>
2018-11-27 08:23:14 +0000

    add server side validation

--- server/routes/form.js
+++ server/routes/form.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { getIn } = require('../utils/functionalHelpers');
+const flash = require('connect-flash');
+const { getIn, isNilOrEmpty, firstItem } = require('../utils/functionalHelpers');
 const { getPathFor } = require('../utils/routes');
 const getFormData = require('../middleware/getFormData');
 const asyncMiddleware = require('../middleware/asyncMiddleware');
@@ -19,6 +20,7 @@ module.exports = function Index({ formService, authenticationMiddleware }) {
 
   router.use(authenticationMiddleware());
   router.use(getFormData(formService));
+  router.use(flash());
 
   router.use((req, res, next) => {
     if (typeof req.csrfToken === 'function') {
@@ -31,22 +33,39 @@ module.exports = function Index({ formService, authenticationMiddleware }) {
     const { section, form } = req.params;
     const backLink = req.get('Referrer');
     const pageData = getIn([section, form], res.locals.formObject);
+    const errors = req.flash('errors');
 
-    res.render(`formPages/${section}/${form}`, { data: pageData, formName: form, backLink });
+    res.render(`formPages/${section}/${form}`, {
+      data: pageData,
+      formName: form,
+      backLink,
+      errors,
+    });
   }));
 
   router.post('/:section/:form', asyncMiddleware(async (req, res) => {
     const { section, form } = req.params;
+    const formPageConfig = formConfig[form];
 
-    await formService.update({
+    const updatedFormObject = await formService.update({
       userId: 'user1',
       formId: res.locals.formId,
       formObject: res.locals.formObject,
-      config: formConfig[form],
+      config: formPageConfig,
       userInput: req.body,
       formSection: section,
       formName: form,
     });
+
+    if (formPageConfig.validate) {
+      const formResponse = getIn([section, form], updatedFormObject);
+      const errors = formService.getValidationErrors(formResponse, formPageConfig);
+
+      if (!isNilOrEmpty(errors)) {
+        req.flash('errors', errors);
+        return res.redirect(`/form/${section}/${form}/`);
+      }
+    }
 
     const nextPath = getPathFor({ data: req.body, config: formConfig[form] });
     res.redirect(`${nextPath}`);
